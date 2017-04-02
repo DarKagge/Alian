e5a73c95b10bb7a52837bb72d1ab6d5925f936f4
jeka <ElectroDeggi33759@yandex.ru>
2017-04-02 23:37:58 +0300

    Old footer delete

--- app/js/common.js
+++ app/js/common.js
@@ -125,3 +125,13 @@ $(".toggle_mnu").click(function() {
 		$(".top_mnu li a").addClass("fadeInUp animated");
 	}
 });
+
+
+
+
+
+
+	$('.open-popup-link').magnificPopup({
+		type:'inline',
+		midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+	});
